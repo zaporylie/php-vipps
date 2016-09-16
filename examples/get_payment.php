98774070e7055a2428c3faa9e9899f00268f91bf
Jakub Piasecki <jakub@nymedia.no>
2016-09-16 14:19:40 +0200

    Add examples

---
+++ examples/get_payment.php
@@ -0,0 +1,30 @@
+<?php
+
+include __DIR__ . '/../vendor/autoload.php';
+$yaml = new \Symfony\Component\Yaml\Parser();
+$settings = \Symfony\Component\Yaml\Yaml::parse(file_get_contents(__DIR__.'/settings.yml'));
+
+try {
+    $client = new \GuzzleHttp\Client([
+        // Set to test server.
+        'base_uri' => $settings['base_uri'],
+        // Add certificate.
+        'cert' => $settings['cert'],
+    ]);
+    $vipps = new \Vipps\Vipps($client);
+    // Set Vipps client.
+    $vipps->setMerchantID($settings['id'])->setMerchantSerialNumber($settings['serialNumber'])->setToken($settings['token']);
+    // Get payment.
+    $payment = $vipps->payments();
+    // Set Order ID.
+    $payment->setOrderID($settings['transaction']['orderId']);
+    // Get transaction status.
+    $payment->getStatus();
+    // Get transaction details.
+    $payment->getDetails();
+    // Dump status.
+    var_dump($payment->getLastResponse());
+}
+catch (Exception $e) {
+    print '<pre>' . var_dump($e) . '</pre>';
+}
---
+++ src/ScriptHandler.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace Vipps;
+
+use Symfony\Component\Filesystem\Filesystem;
+use Symfony\Component\Yaml\Yaml;
+
+class ScriptHandler {
+
+    public static function createExamplesConfigFile()
+    {
+        $fs = new Filesystem();
+        $root = __DIR__ . '/../examples/';
+        if (!$fs->exists($root . 'settings.yml')) {
+            $settings = [
+                'base_uri' => '',
+                'cert' => '',
+                'id' => '',
+                'serialNumber' => '',
+                'token' => '',
+                'transaction' => [
+                    'orderID' => '',
+                    'mobilePhone' => '',
+                    'amount' => 123,
+                    'callback' => '',
+                    'text' => 'test',
+                ],
+            ];
+            $content = Yaml::dump($settings);
+            $fs->dumpFile($root . 'settings.yml', $content);
+        }
+    }
+}
