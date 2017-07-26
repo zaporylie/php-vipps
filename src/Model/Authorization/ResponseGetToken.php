96007de6505dde388e91636734bc471181a65944
Jakub Piasecki <jakub@piaseccy.pl>
2017-07-26 18:14:08 +0200

    Add missing model

---
+++ src/Model/Authorization/ResponseGetToken.php
@@ -0,0 +1,121 @@
+<?php
+
+namespace Vipps\Model\Authorization;
+
+use JMS\Serializer\Annotation as Serializer;
+
+class ResponseGetToken
+{
+
+    /**
+     * @var string
+     * @Serializer\Type("string")
+     */
+    protected $tokenType;
+
+    /**
+     * @var int
+     * @Serializer\Type("integer")
+     */
+    protected $expiresIn;
+
+    /**
+     * @var int
+     * @Serializer\Type("integer")
+     */
+    protected $extExpiresIn;
+
+    /**
+     * @var \DateTimeInterface
+     * @Serializer\Type("DateTime<'U'>")
+     */
+    protected $expiresOn;
+
+    /**
+     * @var \DateTimeInterface
+     * @Serializer\Type("DateTime<'U'>")
+     */
+    protected $notBefore;
+
+    /**
+     * @var string
+     * @Serializer\Type("string")
+     */
+    protected $resource;
+
+    /**
+     * @var string
+     * @Serializer\Type("string")
+     */
+    protected $accessToken;
+
+    /**
+     * Gets accessToken value.
+     *
+     * @return string
+     */
+    public function getAccessToken()
+    {
+        return $this->accessToken;
+    }
+
+    /**
+     * Gets expiresIn value.
+     *
+     * @return int
+     */
+    public function getExpiresIn()
+    {
+        return $this->expiresIn;
+    }
+
+    /**
+     * Gets expiresOn value.
+     *
+     * @return \DateTimeInterface
+     */
+    public function getExpiresOn()
+    {
+        return $this->expiresOn;
+    }
+
+    /**
+     * Gets extExpiresIn value.
+     *
+     * @return int
+     */
+    public function getExtExpiresIn()
+    {
+        return $this->extExpiresIn;
+    }
+
+    /**
+     * Gets notBefore value.
+     *
+     * @return \DateTimeInterface
+     */
+    public function getNotBefore()
+    {
+        return $this->notBefore;
+    }
+
+    /**
+     * Gets resource value.
+     *
+     * @return string
+     */
+    public function getResource()
+    {
+        return $this->resource;
+    }
+
+    /**
+     * Gets tokenType value.
+     *
+     * @return string
+     */
+    public function getTokenType()
+    {
+        return $this->tokenType;
+    }
+}
