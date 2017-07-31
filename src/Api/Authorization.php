1613561af738f85b82ae90ad235044999d84f672
Jakub Piasecki <jakub@piaseccy.pl>
2017-07-31 09:07:32 +0200

    Cleanup Authorization API

--- src/Api/Authorization.php
+++ src/Api/Authorization.php
@@ -2,7 +2,6 @@
 
 namespace Vipps\Api;
 
-use Vipps\Model\Authorization\RequestGetToken;
 use Vipps\Resource\Authorization\GetToken;
 
 class Authorization extends ApiBase implements AuthorizationInterface
@@ -15,11 +14,15 @@ class Authorization extends ApiBase implements AuthorizationInterface
      */
     public function getToken($client_secret)
     {
+        // Initiate GetToken resource.
         $resource = new GetToken($this->app, $this->getSubscriptionKey(), $client_secret);
+
         /** @var \Vipps\Model\Authorization\ResponseGetToken $response */
         $response = parent::doRequest($resource);
-        // Save token in client.
+
+        // Save token on Client for future use.
         $this->app->getClient()->setToken($response->getAccessToken())->setTokenType($response->getTokenType());
+
         return $response;
     }
 }
