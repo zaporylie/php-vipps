58b74e9ace448b168cc83d27722d6efe286642a9
Jakub Piasecki <jakub@piaseccy.pl>
2017-08-01 12:35:57 +0200

    Test requestId generation

---
+++ test/Unit/Resource/RequestIdFactoryTest.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace Vipps\Tests\Unit\Resource;
+
+use PHPUnit\Framework\TestCase;
+use Vipps\Resource\RequestIdFactory;
+
+class RequestIdFactoryTest extends TestCase
+{
+
+    /**
+     * @covers \Vipps\Resource\RequestIdFactory::generate()
+     */
+    public function testGenerate()
+    {
+        $request_id = RequestIdFactory::generate();
+        $this->assertLessThan(30, strlen($request_id));
+    }
+}
---
+++ test/Unit/Resource/ResourceTestBase.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace Vipps\Tests\Unit\Resource;
+
+use Http\Client\HttpClient;
+use PHPUnit\Framework\TestCase;
+use Vipps\Client;
+use Vipps\Vipps;
+
+abstract class ResourceTestBase extends TestCase
+{
+
+    /**
+     * @var \Vipps\Vipps
+     */
+    protected $vipps;
+
+    /**
+     * @var \Vipps\Client
+     */
+    protected $client;
+
+    /**
+     * @var \Http\Client\HttpClient
+     */
+    protected $httpClient;
+
+    /**
+     * {@inheritdoc}
+     */
+    protected function setUp()
+    {
+        parent::setUp();
+        $this->httpClient = $this->createMock(HttpClient::class);
+        $this->client = new Client('test_client_id', [
+            'http_client' => $this->httpClient,
+        ]);
+        $this->vipps = new Vipps($this->client);
+    }
+}
