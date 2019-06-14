c5c8b1962f7ec2841e2b26835fa0c64568c2ad34
Jakub Piasecki <jakub@unity3d.com>
2019-06-14 14:55:06 +0300

    Improve test coverage for getPaymentDetails

--- test/Integration/Api/PaymentsTest.php
+++ test/Integration/Api/PaymentsTest.php
@@ -3,6 +3,11 @@
 namespace zaporylie\Vipps\Tests\Integration\Api;
 
 use zaporylie\Vipps\Exceptions\VippsException;
+use zaporylie\Vipps\Model\Payment\Address;
+use zaporylie\Vipps\Model\Payment\PaymentShippingDetails;
+use zaporylie\Vipps\Model\Payment\TransactionLog;
+use zaporylie\Vipps\Model\Payment\TransactionSummary;
+use zaporylie\Vipps\Model\Payment\UserDetails;
 use zaporylie\Vipps\Tests\Integration\IntegrationTestBase;
 
 /**
@@ -253,20 +258,45 @@ class PaymentsTest extends IntegrationTestBase
         // Mock response.
         $this->mockResponse(parent::getResponse([
             'orderId' => 'test_order_id',
+            'shippingDetails' => [
+                'address' => [
+                    'addressLine1' => 'Dronning Eufemias gate 42',
+                    'addressLine2' => 'Att: Rune Garborg',
+                    'city' => 'Oslo',
+                    'country' => 'Norway',
+                    'postCode' => '0191'
+                ],
+                'shippingCost' => 1500,
+                'shippingMethod' => 'Posten',
+                'shippingMethodId' => 'string'
+            ],
+            'transactionLogHistory' => [
+                [
+                    'amount' => 1200,
+                    'operation' => 'RESERVE',
+                    'operationSuccess' => true,
+                    'requestId' => 'test_request_id',
+                    'timeStamp' => '2017-07-31T15:07:37.0Z',
+                    'transactionId' => 'test_transaction_id',
+                    'transactionText' => 'test_transaction_text'
+                ],
+            ],
             'transactionSummary' => [
                 'capturedAmount' => 10,
-                'remainingAmountToCapture' => 11,
                 'refundedAmount' => 12,
+                'remainingAmountToCapture' => 11,
                 'remainingAmountToRefund' => 13,
             ],
-            'transactionLogHistory' => [[
-                'amount' => 1200,
-                'transactionText' => 'test_transaction_text',
-                'timeStamp' => '2017-07-31T15:07:37.100Z',
-                'operationSuccess' => true,
-                'transactionId' => 'test_transaction_id',
-                'requestId' => 'test_request_id',
-            ]],
+            'userDetails' => [
+                'bankIdVerified' => 'Y',
+                'dateOfBirth' => '12-3-1988',
+                'email' => 'user@example.com',
+                'firstName' => 'Ada',
+                'lastName' => 'Lovelace',
+                'mobileNumber' => '12345678',
+                'ssn' => '12345678901',
+                'userId' => 'uiJskNQ6qNN1iwN891uuob=='
+            ]
         ]));
 
         // Do request.
@@ -276,18 +306,44 @@ class PaymentsTest extends IntegrationTestBase
 
         // Assert response.
         $this->assertEquals('test_order_id', $response->getOrderId());
+
+        $this->assertInstanceOf(PaymentShippingDetails::class, $response->getShippingDetails());
+        $this->assertInstanceOf(Address::class, $response->getShippingDetails()->getAddress());
+        $this->assertEquals('Dronning Eufemias gate 42', $response->getShippingDetails()->getAddress()->getAddressLine1());
+        $this->assertEquals('Att: Rune Garborg', $response->getShippingDetails()->getAddress()->getAddressLine2());
+        $this->assertEquals('Oslo', $response->getShippingDetails()->getAddress()->getCity());
+        $this->assertEquals('Norway', $response->getShippingDetails()->getAddress()->getCountry());
+        $this->assertEquals('0191', $response->getShippingDetails()->getAddress()->getPostCode());
+        $this->assertEquals(1500, $response->getShippingDetails()->getShippingCost());
+        $this->assertEquals('Posten', $response->getShippingDetails()->getShippingMethod());
+        $this->assertEquals('string', $response->getShippingDetails()->getShippingMethodId());
+
+        $this->assertInstanceOf(TransactionSummary::class, $response->getTransactionSummary());
         $this->assertEquals(10, $response->getTransactionSummary()->getCapturedAmount());
         $this->assertEquals(11, $response->getTransactionSummary()->getRemainingAmountToCapture());
         $this->assertEquals(12, $response->getTransactionSummary()->getRefundedAmount());
         $this->assertEquals(13, $response->getTransactionSummary()->getRemainingAmountToRefund());
+
+        $this->assertInstanceOf(TransactionLog::class, $response->getTransactionLogHistory()[0]);
         $this->assertEquals(1200, $response->getTransactionLogHistory()[0]->getAmount());
-        $this->assertEquals('test_transaction_text', $response->getTransactionLogHistory()[0]->getTransactionText());
+        $this->assertEquals('RESERVE', $response->getTransactionLogHistory()[0]->getOperation());
+        $this->assertEquals(true, $response->getTransactionLogHistory()[0]->getOperationSuccess());
+        $this->assertEquals('test_request_id', $response->getTransactionLogHistory()[0]->getRequestId());
         $this->assertEquals(
             '2017-07-31T15:07:37',
             $response->getTransactionLogHistory()[0]->getTimeStamp()->format('Y-m-d\TH:i:s')
         );
-        $this->assertEquals(true, $response->getTransactionLogHistory()[0]->getOperation());
         $this->assertEquals('test_transaction_id', $response->getTransactionLogHistory()[0]->getTransactionId());
-        $this->assertEquals('test_request_id', $response->getTransactionLogHistory()[0]->getRequestId());
+        $this->assertEquals('test_transaction_text', $response->getTransactionLogHistory()[0]->getTransactionText());
+
+        $this->assertInstanceOf(UserDetails::class, $response->getUserDetails());
+        $this->assertEquals('Y', $response->getUserDetails()->getBankIdVerified());
+        $this->assertEquals('12-3-1988', $response->getUserDetails()->getDateOfBirth());
+        $this->assertEquals('user@example.com', $response->getUserDetails()->getEmail());
+        $this->assertEquals('Ada', $response->getUserDetails()->getFirstName());
+        $this->assertEquals('Lovelace', $response->getUserDetails()->getLastName());
+        $this->assertEquals('12345678', $response->getUserDetails()->getMobileNumber());
+        $this->assertEquals('12345678901', $response->getUserDetails()->getSsn());
+        $this->assertEquals('uiJskNQ6qNN1iwN891uuob==', $response->getUserDetails()->getUserId());
     }
 }
