ba2645955950b60377c9f9f18e4bb0bb890584cb
Jakub Piasecki <jakub@piaseccy.pl>
2021-09-22 23:27:12 +0200

    Add getters for user info

--- src/Model/UserInfo/ResponseUserInfo.php
+++ src/Model/UserInfo/ResponseUserInfo.php
@@ -83,4 +83,124 @@ class ResponseUserInfo
      * @Serializer\Type("string")
      */
     protected $sub;
+
+    /**
+     * Gets accounts value.
+     *
+     * @return \zaporylie\Vipps\Model\UserInfo\AccountInfo[]
+     */
+    public function getAccounts()
+    {
+        return $this->accounts;
+    }
+
+    /**
+     * Gets address value.
+     *
+     * @return \zaporylie\Vipps\Model\UserInfo\Address
+     */
+    public function getAddress()
+    {
+        return $this->address;
+    }
+
+    /**
+     * Gets birthday value.
+     *
+     * @return string
+     */
+    public function getBirthday()
+    {
+        return $this->birthday;
+    }
+
+    /**
+     * Gets email value.
+     *
+     * @return string
+     */
+    public function getEmail()
+    {
+        return $this->email;
+    }
+
+    /**
+     * Gets family_name value.
+     *
+     * @return string
+     */
+    public function getFamilyName()
+    {
+        return $this->family_name;
+    }
+
+    /**
+     * Gets given_name value.
+     *
+     * @return string
+     */
+    public function getGivenName()
+    {
+        return $this->given_name;
+    }
+
+    /**
+     * Gets name value.
+     *
+     * @return string
+     */
+    public function getName()
+    {
+        return $this->name;
+    }
+
+    /**
+     * Gets nin value.
+     *
+     * @return string
+     */
+    public function getNin()
+    {
+        return $this->nin;
+    }
+
+    /**
+     * Gets other_addresses value.
+     *
+     * @return \zaporylie\Vipps\Model\UserInfo\Address[]
+     */
+    public function getOtherAddresses()
+    {
+        return $this->other_addresses;
+    }
+
+    /**
+     * Gets phone_number value.
+     *
+     * @return string
+     */
+    public function getPhoneNumber()
+    {
+        return $this->phone_number;
+    }
+
+    /**
+     * Gets sid value.
+     *
+     * @return string
+     */
+    public function getSid()
+    {
+        return $this->sid;
+    }
+
+    /**
+     * Gets sub value.
+     *
+     * @return string
+     */
+    public function getSub()
+    {
+        return $this->sub;
+    }
 }
