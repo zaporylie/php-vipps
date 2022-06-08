6fc6f7f0254436484e07235ec8e964e0fdb39ee9
Pelle Marius Jensen <pellejdev@gmail.com>
2022-06-08 12:57:26 +0200

    CampaignRequest: Change end datetime format and add getset

--- src/Model/RecurringPayment/CampaignRequest.php
+++ src/Model/RecurringPayment/CampaignRequest.php
@@ -19,7 +19,55 @@ class CampaignRequest
 
     /**
      * @var \DateTimeInterface
-     * @Serializer\Type("DateTime<'Y-m-d\TH:i:s.u\Z'>")
+     * @Serializer\Type("DateTime<'Y-m-d\TH:i:s\Z'>")
      */
     protected $end;
+
+    /**
+     * Get the value of end
+     *
+     * @return  \DateTimeInterface
+     */ 
+    public function getEnd()
+    {
+        return $this->end;
+    }
+
+    /**
+     * Set the value of end
+     *
+     * @param  \DateTimeInterface  $end
+     *
+     * @return  self
+     */ 
+    public function setEnd(\DateTimeInterface $end)
+    {
+        $this->end = $end;
+
+        return $this;
+    }
+
+    /**
+     * Get the value of campaignPrice
+     *
+     * @return  int
+     */ 
+    public function getCampaignPrice()
+    {
+        return $this->campaignPrice;
+    }
+
+    /**
+     * Set the value of campaignPrice
+     *
+     * @param  int  $campaignPrice
+     *
+     * @return  self
+     */ 
+    public function setCampaignPrice(int $campaignPrice)
+    {
+        $this->campaignPrice = $campaignPrice;
+
+        return $this;
+    }
 }
