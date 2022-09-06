947d7a866056d33e02e49251a906a2e7a9484e8a
Ivan Ruzak <ivan@drupfan.com>
2022-09-06 16:01:56 +0300

    Checkout: fixed issue with serializer

--- src/Resource/Checkout/CheckoutResourceBase.php
+++ src/Resource/Checkout/CheckoutResourceBase.php
@@ -2,6 +2,9 @@
 
 namespace zaporylie\Vipps\Resource\Checkout;
 
+use JMS\Serializer\Naming\IdenticalPropertyNamingStrategy;
+use JMS\Serializer\Naming\SerializedNameAnnotationStrategy;
+use JMS\Serializer\SerializerBuilder;
 use zaporylie\Vipps\Resource\ResourceBase;
 use zaporylie\Vipps\VippsInterface;
 
@@ -19,6 +22,11 @@ abstract class CheckoutResourceBase extends ResourceBase
     public function __construct(VippsInterface $vipps, string $subscription_key, string $client_secret)
     {
         parent::__construct($vipps, $subscription_key);
+
+        // Adjust serializer.
+        $this->serializer = SerializerBuilder::create()
+            ->setPropertyNamingStrategy(new SerializedNameAnnotationStrategy(new IdenticalPropertyNamingStrategy()))
+            ->build();
 
         // Client id and secret should be set in headers.
         $this->headers['client_id'] = $this->app->getClient()->getClientId();
